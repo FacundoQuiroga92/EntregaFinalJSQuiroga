4a2354f498848bb98cf40f1537716b535fc9ce37
facundo <faquu92@gmail.com>
2022-12-29 22:06:29 -0300

    3ra pre entrega

--- scripts/miArchivo.js
+++ scripts/miArchivo.js
@@ -1,55 +1,90 @@
-/* SIMULACION DE TIENDA DE GORRAS Y TERMOS DE EQUIPOS DE FUTBOL */
+let nombreForm = document.querySelector("#nombre");
+let correoForm = document.querySelector("#correo");
 
-/*Bienvenida con entrada de datos*/
-function bienvenida(){
-    let nombre = prompt("Por favor ingrese su nombre")
-    alert(`bienvenido a mundo deportivo ${nombre}`)
-}
-
-bienvenida()
-
-/* utilizacion de constructor */
-class cliente {
-    constructor(nombre, correo){
-        this.nombre = nombre;
-        this.correo = correo;
-    }
-    mostrarValidacion(){
-        console.log("usuario:"+this.nombre+" tu correo:"+this.correo+" fue validado con exito")
-    }
-}
-
-const cliente1 = new cliente ("lionel", "lio10@gmail.com")
-cliente1.mostrarValidacion()
-
-/* Utilizacion de array con objetos */
-const productos = [ 
-    {id:1, nombre: "gorra de river", precio: 2000 },
-    {id:2, nombre: "gorra de boca", precio: 2000 },
-    {id:3, nombre: "termo de boca" , precio : 3500},
-    {id:4, nombre: "termo de river" , precio: 3000},
-    {id:5, nombre: "termo de racing" , precio: 1100},
-    {id:6, nombre: "gorra de independiente" , precio: 1100},
-    {id:7, nombre: "gorra de gimnasia de la plata" , precio: 800},
-    {id:8, nombre: "termo de estudiantes" , precio: 1000},
+// Transformamos a Json el array de objetos y subida a localStorage
+let clientesDatos=[
+    {nombre:"Carlos",correo:"carlos@gmail.com"},
+    {nombre:"Juan",correo:"Juan2022@gmail.com"}
 ]
+const aJson = JSON.stringify(clientesDatos)
 
-/* utilizacion de metodos */
-productos.forEach((producto) => {
-    console.log (producto.nombre);
-    console.log (producto.precio);
+localStorage.setItem("clientesDatos", aJson)
+
+//eventos
+nombreForm.addEventListener("input", function(){
+    if (nombreForm.value===""){
+        alert("Ingrese un nombre por favor")
+    }
+});
+correoForm.addEventListener("input", function(){
+    if (correoForm.value===""){
+        alert("ingrese un correo valido por favor")
+    }
+});
+ 
+let formulario = document.querySelector("#formulario")
+
+let info = document.querySelector("#printForm")
+
+// mostrar info del form, bajada de array desde el local storage, push de datos ingresados del imput y subida de nuevo al local storage como archivo json
+
+const printInfo= formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+    info.innerHTML=`
+    <div class="alert alert-primary" role=alert">
+    <h5>Gracias ${nombreForm.value} por dejarnos tus datos, te contactaremos a ${correoForm.value} para solucionar tus dudas</h5>
+    </div>
+    `
+    const usuario = {
+        nombre: `${nombre.value}`,
+        correo: `${correo.value}`
+    }
+    
+    const agregarUsu= JSON.parse(localStorage.getItem("clientesDatos"))
+        agregarUsu.push(usuario)
+        localStorage.setItem("datosClientes", JSON.stringify(agregarUsu))
+
 })
 
-let gorras = productos.filter((p) => p.nombre.includes ("gorra")) ;
-console.log(gorras);
+//buscador de productos
+const productos = [
+    {id: 12, nombre: 'CAMISETA BOCA', importe: 30000},
+    {id: 11, nombre: 'CAMISETA RIVER', importe: 30000},
+    {id: 47, nombre: 'GORRA BOCA ', importe: 5000},
+    {id: 20, nombre: 'GORRA RACING', importe: 5000},
+    {id: 50, nombre: 'CAMISETA BRASIL', importe: 25000},
+    {id: 58, nombre: 'PELOTA MUNDIAL 2022', importe: 15000}]
 
-let noTermoR = productos.filter((p) => p.nombre !== "termo de racing") ;
-console.log(noTermoR);
-
-let mejorPrecio = productos.find((p)=>p.precio < 1000)
-console.log(mejorPrecio)
-
-/* total de la compra */
-const totalCarrito = productos.reduce((acumulador,producto) => acumulador + producto.precio, 0)
-console.log(totalCarrito);
-
+    const inputBusqueda = document.querySelector("#inputSearch")
+    const tablebody = document.querySelector("tbody")
+    
+    
+    const armarTabla = (prod)=> {
+        return `<tr>
+                    <td>${prod.id}</td>
+                    <td>${prod.nombre}</td>
+                    <td>${prod.importe}</td>
+                </tr>`
+    }
+    
+    const filtrarProductos = ()=> {
+        let parametro = inputBusqueda.value.trim().toUpperCase()
+        let resultado = productos.filter(prod => prod.nombre.includes(parametro))
+            if (resultado.length > 0) {
+                subirProductos(resultado)
+            }
+    }
+    
+    const subirProductos = (array)=> {
+        let tabla = ""
+        if (array.length > 0) {
+            array.forEach(prod => {
+                tabla += armarTabla(prod)
+            })
+            tablebody.innerHTML = tabla
+        }
+    }
+    subirProductos(productos)
+    
+    inputBusqueda.addEventListener("search", filtrarProductos)
+    
